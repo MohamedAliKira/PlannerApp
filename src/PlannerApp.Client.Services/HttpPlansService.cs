02d231b0a5bfb6ababce50c6a826f4b9db741f70
Mohamed Ali <mohamed.ali.kira@gmail.com>
2021-08-23 16:16:14 +0100

    Fetch Plans 1 - Create the Models & Http Client Service

---
+++ src/PlannerApp.Client.Services/HttpPlansService.cs
@@ -0,0 +1,37 @@
+﻿using PlannerApp.Client.Services.Exceptions;
+using PlannerApp.Client.Services.Interfaces;
+using PlannerApp.Shared.Models;
+using PlannerApp.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Client.Services
+{
+    public class HttpPlansService : IPlansService
+    {
+        private readonly HttpClient _client;
+        public HttpPlansService(HttpClient client)
+        {
+            _client = client;
+        }
+        public async Task<ApiResponse<PageList<PlanSummary>>> GetPlansAsync(string query = null, int pageNumber = 1, int pageSize = 10)
+        {
+            var response = await _client.GetAsync($"/api/v2/plans?query={query}&pageNumber={pageNumber}&pageSize={pageSize}");
+            if(response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<ApiResponse<PageList<PlanSummary>>>();
+                return result;
+            }
+            else
+            {
+                var errorResponse = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                throw new ApiException(errorResponse, response.StatusCode);
+            }
+        }
+    }
+}
