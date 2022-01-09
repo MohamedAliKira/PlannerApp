e83b8375d5e587df2822163eab4afed3db07af38
BOUALAMALLAH Mohamed Ali <mohamed.ali.kira@gmail.com>
2022-01-09 14:48:06 +0100

    Implement MudBlazor Themes for Dark & Light Modes (Themes)

--- src/PlannerApp/Shared/MainLayout.razor.cs
+++ src/PlannerApp/Shared/MainLayout.razor.cs
@@ -1,33 +1,67 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-using System.Net.Http.Json;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.JSInterop;
-using PlannerApp;
-using PlannerApp.Shared;
-using PlannerApp.Components;
 using MudBlazor;
-using Blazored.FluentValidation;
-using PlannerApp.Pages.Authentication;
 
 namespace PlannerApp.Shared
 {
     public partial class MainLayout
     {
+        [Inject] ILocalStorageService LocalStorage { get; set; }
+
         bool _drawerOpen = true;
+        
         void DrawerToggle()
         {
             _drawerOpen = !_drawerOpen;
+        }
+        protected async override Task OnInitializedAsync()
+        {
+            if (await LocalStorage.ContainKeyAsync("theme"))
+                _themeName = await LocalStorage.GetItemAsStringAsync("theme");
+                
+            _currentTheme = _themeName == "light" ? _lightTheme : _darkTheme; //_darkTheme;
+        }
+
+        MudTheme _currentTheme = null;
+
+        MudTheme _darkTheme = new MudTheme
+        {
+            Palette = new Palette
+            {
+                Black = "#27282f",
+                Background = "#32343d",
+                BackgroundGrey = "#27282f",
+                Surface = "#575967",
+                TextPrimary = "#ffffff",
+                TextSecondary = "rgba(255,255,255, 0.50)",
+                DrawerBackground = "#000000",
+                DrawerText = "#ffffff",
+                Primary = "#007CD1",
+                AppbarBackground = "#181818",
+                AppbarText = "#ffffff",
+                ActionDisabled = "rgba(255,255,255, 0.30)",
+                TextDisabled = "rgba(255,255,255, 0.30)"
+            }
+        };
+
+        MudTheme _lightTheme = new MudTheme();
+
+        private string _themeName = "light";
+        
+        private async Task ToggleThemeAsync()
+        {
+            if(_themeName == "light")
+            {
+                _currentTheme = _darkTheme;
+                _themeName = "dark";
+            }
+            else
+            {
+                _currentTheme = _lightTheme;
+                _themeName = "light";                
+            }
+
+            await LocalStorage.SetItemAsStringAsync("theme", _themeName);
         }
     }
 }
